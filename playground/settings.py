bdf9bdf7c4b46fc7263ffb65756431acc6b77056
Mason Fisher <sackzombie10@gmail.com>
2020-02-08 09:08:15 -0500

    Made HTML page show up

--- playground/settings.py
+++ playground/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'playground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['playground/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
